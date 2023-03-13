0c176aa6aa08a24228ec0d22001b33f5c2e66d4d
Tobiasz2817 <Tobiasz2817@outlook.com>
2023-03-13 12:35:07 +0100

    Add new input system and joystick

---
+++ Assets/InputManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    private InputActions _inputs;
+
+    private void Awake()
+    {
+        _inputs = new InputActions();
+    }
+
+    private void OnEnable()
+    {
+        var playerController = FindObjectOfType<PlayerController>();
+        if (playerController == null) return;
+        
+        _inputs.Character.MoveJoystick.performed += moveValue =>
+        {
+            playerController.MovementValue = moveValue.ReadValue<Vector2>();
+        };
+        _inputs.Character.MoveJoystick.canceled += moveValue =>
+        {
+            playerController.MovementValue = moveValue.ReadValue<Vector2>();
+        };
+        _inputs.Character.Jump.performed += jumpValue =>
+        {
+            playerController.IsJumping = jumpValue.ReadValueAsButton();
+        };
+        _inputs.Character.Sprint.performed += sprintValue =>
+        {
+            playerController.IsSprinting = sprintValue.ReadValueAsButton();
+        };
+        
+        _inputs.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _inputs.Disable();
+    }
+}
