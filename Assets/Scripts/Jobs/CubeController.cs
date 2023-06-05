9214b4cff8e786a237634a4eb6d317f772eddef0
Tobiasz2817 <Tobiasz2817@outlook.com>
2023-06-05 13:20:57 +0200

    Fix Job System

--- Assets/Scripts/Jobs/CubeController.cs
+++ Assets/Scripts/Jobs/CubeController.cs
@@ -1,68 +1,30 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.Collections;
 using UnityEngine;
 
 public class CubeController : MonoBehaviour
 {
-    private ObjectsGenerator _objectsGenerator;
-    
-    public void InitCubeController(ObjectsGenerator objectsGenerator_) {
-        _objectsGenerator = objectsGenerator_;
-    }
-    
-    private void CalculateNeighbors()
-    {
-        FindClosestNeighbors();
-        FindFarthestNeighbor();
-    }
-
-    private void FindClosestNeighbors()
-    {
-        Vector3 cubePosition = transform.position;
-        var cubes = new List<Vector3>(_objectsGenerator.cubePositions);
-
-        cubes.Sort((cube1, cube2) =>
-        {
-            float distance1 = Vector3.Distance(cube1, cubePosition);
-            float distance2 = Vector3.Distance(cube2, cubePosition);
-            return distance1.CompareTo(distance2);
-        });
-
-        int neighborCount = Mathf.Min(cubes.Count - 1, 3);
-        for (int i = 0; i < neighborCount; i++)
-        {
-            Vector3 neighborPosition = cubes[i + 1];
-            DrawNeighborLine(transform.position, neighborPosition, Color.green);
-        }
-    }
-
-    private void FindFarthestNeighbor()
-    {
-        Vector3 cubePosition = transform.position;
-        var cubes = new List<Vector3>(_objectsGenerator.cubePositions);
-
-        cubes.Sort((cube1, cube2) =>
-        {
-            float distance1 = Vector3.Distance(cube1, cubePosition);
-            float distance2 = Vector3.Distance(cube2, cubePosition);
-            return distance2.CompareTo(distance1);
-        });
-
-        Vector3 farthestNeighborPosition = cubes[0];
-        DrawNeighborLine(transform.position, farthestNeighborPosition, Color.red);
-    }
-
-    private void DrawNeighborLine(Vector3 cubePosition, Vector3 neighborPosition, Color color)
-    {
-        Debug.DrawLine(cubePosition, neighborPosition, color);
-    }
+    public Vector3[] Closest { get; set; }
+    public Vector3 Farthest { get; set; }
 
     private void OnDrawGizmosSelected()
     {
-        CalculateNeighbors();
+        if (Closest is not null)
+        {
+            Gizmos.color = Color.green;
+            for (int i = 0; i < 3; i++)
+                Gizmos.DrawLine(transform.position, Closest[i]);
+        }
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, Farthest);
+
     }
 }
 
 
+public struct ClosestAndFarthest
+{
+    public int first;
+    public int second;
+    public int third;
+    public int farthest;
+}
--- Assets/Scripts/Jobs/ObjectsGenerator.cs
+++ Assets/Scripts/Jobs/ObjectsGenerator.cs
@@ -1,65 +1,149 @@
+using System.Collections.Generic;
 using UnityEngine;
 using Unity.Jobs;
 using Unity.Collections;
 using Unity.Burst;
-using Random = Unity.Mathematics.Random;
+using Unity.Mathematics;
 
 public class ObjectsGenerator : MonoBehaviour
 {
-    public GameObject cubePrefab;
-    public int minCubeCount = 1000;
-    public int maxCubeCount = 10000;
-    public float sphereRadius = 10f;
+        [SerializeField] private CubeController prefab;
+        [SerializeField] private int countSpawnObjects = 1000;
+        
+        private List<CubeController> _spawnedCubes = new List<CubeController>();
+        
+        private NativeArray<float3> _cubesPosition;
+        private NativeArray<ClosestAndFarthest> closestAndFarthests;
+        private FindClosestJob _findClosestJob;
+        private JobHandle _handler;
+        
 
-    public NativeArray<Vector3> cubePositions;
-
-    private void Start()
-    {
-        GenerateCubes();
-    }
-
-    private void OnDestroy()
-    {
-        cubePositions.Dispose();
-    }
-
-    private void GenerateCubes()
-    {
-        int cubeCount = UnityEngine.Random.Range(minCubeCount, maxCubeCount);
-
-        cubePositions = new NativeArray<Vector3>(cubeCount, Allocator.Persistent);
-
-        Random random = new Random((uint)System.Environment.TickCount);
-
-        CubeGeneratorJob generatorJob = new CubeGeneratorJob()
+        private void Awake() => Spawn();
+        
+        private void Update()
         {
-            cubePositions = this.cubePositions,
-            random = random,
-            sphereRadius = sphereRadius
-        };
-
-        JobHandle jobHandle = generatorJob.Schedule(cubeCount, 64);
-        jobHandle.Complete();
-
-        for (int i = 0; i < cubeCount; i++)
-        {
-            GameObject cube = Instantiate(cubePrefab, cubePositions[i], Quaternion.identity);
-            cube.GetComponent<CubeController>().InitCubeController(this);
+            UpdatePositionDots();
         }
-    }
 
-    [BurstCompile]
-    private struct CubeGeneratorJob : IJobParallelFor
-    {
-        public NativeArray<Vector3> cubePositions;
-
-        public Random random;
-        public float sphereRadius;
-
-        public void Execute(int index)
+        private void LateUpdate()
         {
-            Vector3 position = random.NextFloat3(-sphereRadius, sphereRadius);
-            cubePositions[index] = position;
+            _handler.Complete();
+
+            var spawnedDotsCubeCount = _spawnedCubes.Count;
+            var result = _findClosestJob.result;
+
+            for (int i = 0; i < spawnedDotsCubeCount; i++)
+            {
+                _spawnedCubes[i].Closest = new[] 
+                {
+                    _spawnedCubes[result[i].first].transform.position,
+                    _spawnedCubes[result[i].second].transform.position,
+                    _spawnedCubes[result[i].third].transform.position,
+                };
+                _spawnedCubes[i].Farthest = _spawnedCubes[result[i].farthest].transform.position;
+            }
         }
-    }
+        
+        private void Spawn()
+        {
+            for(int i = 0; i < countSpawnObjects; i++)
+                _spawnedCubes.Add(Instantiate(prefab, UnityEngine.Random.insideUnitSphere * 100f, Quaternion.identity, transform));
+        }
+
+        private void UpdatePositionDots()
+        {
+            var spawnedDotsCubesCount = _spawnedCubes.Count;
+
+            if (_cubesPosition.IsCreated)
+                _cubesPosition.Dispose();
+
+            if (closestAndFarthests.IsCreated)
+                closestAndFarthests.Dispose();
+
+            _cubesPosition = new NativeArray<float3>(spawnedDotsCubesCount, Allocator.TempJob, NativeArrayOptions.UninitializedMemory);
+            closestAndFarthests = new NativeArray<ClosestAndFarthest>(spawnedDotsCubesCount, Allocator.TempJob, NativeArrayOptions.UninitializedMemory);
+
+            for(int i = 0; i < spawnedDotsCubesCount; i++)
+                _cubesPosition[i] = _spawnedCubes[i].transform.position;
+
+            _findClosestJob = new FindClosestJob
+            {
+                length = spawnedDotsCubesCount,
+                cubesPosition = _cubesPosition,
+                result = closestAndFarthests
+            };
+
+            _handler = _findClosestJob.Schedule();
+        }
+
+        [BurstCompile]
+        private struct FindClosestJob : IJob
+        {
+            public int length;
+            [ReadOnly] public NativeArray<float3> cubesPosition;
+            [WriteOnly] public NativeArray<ClosestAndFarthest> result;
+
+            public void Execute()
+            {
+                for (int i = 0; i < length; i++)
+                    result[i] = GetClosestAndFarthest(i, cubesPosition[i]);
+                
+            }
+
+            private ClosestAndFarthest GetClosestAndFarthest(int currentIndex, float3 currentPosition)
+            {
+                var closestDistances = new NativeArray<float>(3, Allocator.Temp, NativeArrayOptions.UninitializedMemory);
+                var closestIndices = new NativeArray<int>(3, Allocator.Temp, NativeArrayOptions.UninitializedMemory);
+                
+                var farthestDistance = float.MinValue;
+                var farthestIndex = -1;
+
+                for (int i = 0; i < 3; i++)
+                {
+                    closestDistances[i] = float.MaxValue;
+                    closestIndices[i] = -1;
+                }
+                
+                for (int i = 0; i < length; i++)
+                {
+                    if (i == currentIndex)
+                        continue;
+                    
+                    var distance = math.distance(currentPosition, cubesPosition[i]);
+
+                    // Check if the current distance is smaller than the stored distances
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (distance < closestDistances[j])
+                        {
+                            // Shift the other closest distances and indices
+                            for (int k = 3 - 1; k > j; k--)
+                            {
+                                closestDistances[k] = closestDistances[k - 1];
+                                closestIndices[k] = closestIndices[k - 1];
+                            }
+
+                            // Store the new closest distance and index
+                            closestDistances[j] = distance;
+                            closestIndices[j] = i;
+                            break;
+                        }
+                    }
+                    // Check if the current distance is larger than the farthest distance
+                    if (distance > farthestDistance)
+                    {
+                        farthestDistance = distance;
+                        farthestIndex = i;
+                    }
+                }
+
+                return new ClosestAndFarthest()
+                {
+                    first = closestIndices[0],
+                    second = closestIndices[1],
+                    third = closestIndices[2],
+                    farthest = farthestIndex
+                };
+            }
+        }
 }
